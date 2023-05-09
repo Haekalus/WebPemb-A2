d318046d377c70baac69f549bbb8116a661d2740
Haekalus <126176613+Haekalus@users.noreply.github.com>
2023-05-09 09:42:00 +0700

    Add files via upload

---
+++ _JS/Script/Js.js
@@ -0,0 +1,27 @@
+document.writeln("Hello World")
+document.writeln("<br/>");
+document.writeln("NAMA : " + "  Agam ");
+document.write("<br/>");
+
+let fullName = "Muhammad Hussein Haekal";
+let firstName = "Muhammad";
+let middleName = "Hussein";
+let lastName = "Haekal";
+
+document.writeln("Nama Lengakap : " + fullName);
+document.write("<br/>");
+document.writeln("Nama Pertama : " + firstName);
+document.write("<br/>");
+document.writeln("Nama Tengah : " + middleName);
+document.write("<br/>");
+document.writeln("Nama Akhir : " + lastName);
+document.writeln("<br/>");
+
+let result = 1 + 2;
+document.writeln;
+
+let counter = 1;
+for (; counter <= 10; ) {
+  document.writeln("<p>Perulangan ke " + counter + "</p>");
+  counter++;
+}
